fd3caba76bda397341dcbbfa474a55e72db2ea22
hywenklis <hywenklis@hotmail.com>
2023-03-05 02:22:31 -0300

    ajustes

--- logic.js
+++ logic.js
@@ -1,13 +1,13 @@
 const form = document.getElementById("search-form");
-form.addEventListener("submit", function (event) {
-    event.preventDefault(); // previne o envio do formulário
-    const query = form.elements.search.value; // obtém o valor do campo de pesquisa
-    const cards = document.querySelectorAll(".card"); // obtém todos os cards
-    for (let card of cards) {
-        if (card.querySelector(".card-title").textContent.includes(query)) {
-            card.style.display = "block"; // exibe o card se a descrição inclui a consulta
-        } else {
-            card.style.display = "none"; // oculta o card caso contrário
-        }
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // previne o envio do formulário
+  const query = form.elements.search.value; // obtém o valor do campo de pesquisa
+  const cards = document.querySelectorAll(".card"); // obtém todos os cards
+  for (let card of cards) {
+    if (card.querySelector(".card-title").textContent.includes(query)) {
+      card.style.display = "block"; // exibe o card se a descrição inclui a consulta
+    } else {
+      card.style.display = "none"; // oculta o card caso contrário
     }
+  }
 });
