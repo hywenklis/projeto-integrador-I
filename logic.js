a9f520546a31b4237a3c7913ba4402f0ac1db49c
hywenklis <hywenklis@hotmail.com>
2023-03-05 02:08:24 -0300

    adiciona cards

--- logic.js
+++ logic.js
@@ -4,7 +4,7 @@ form.addEventListener("submit", function (event) {
     const query = form.elements.search.value; // obtém o valor do campo de pesquisa
     const cards = document.querySelectorAll(".card"); // obtém todos os cards
     for (let card of cards) {
-        if (card.querySelector(".card-title").textContent.includes(query)) {
+        if (card.querySelector(".card-description").textContent.includes(query)) {
             card.style.display = "block"; // exibe o card se a descrição inclui a consulta
         } else {
             card.style.display = "none"; // oculta o card caso contrário
